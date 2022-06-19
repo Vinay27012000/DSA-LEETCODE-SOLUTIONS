f43169db69fc071b369c7073a74746940c2616b2
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2022-06-19 15:44:12 +0530

    Time: 6 ms (60.63%), Space: 43.5 MB (64.91%) - LeetHub

---
+++ 242-valid-anagram/242-valid-anagram.java
@@ -0,0 +1,22 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        if(s.length()!=t.length())
+        {
+            return false;
+        }
+        int[] arr=new int[26];
+        for(int i=0;i<s.length();i++)
+        {
+            char c1=s.charAt(i);
+            char c2=t.charAt(i);
+            arr[c1-'a']++;
+            arr[c2-'a']--;
+        }
+        for(int i=0;i<26;i++)
+        {
+            if(arr[i]!=0)
+                return false;
+        }
+        return true;
+    }
+}
