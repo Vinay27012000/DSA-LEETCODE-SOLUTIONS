9e7531a109ca79fccea302b68f29c976ead1656e
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2022-07-31 21:49:53 +0530

    Time: 1 ms (12.77%), Space: 43.6 MB (5.78%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        int peak=0;
+        int max=0;
+        
+        for(int i=1 ;i<nums.length;i++){
+            if(nums[i]>nums[i-1]){
+                peak=i;
+            }
+        }
+        return peak;
+    }
+}
