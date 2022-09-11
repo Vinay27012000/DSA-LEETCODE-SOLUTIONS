6e2c5d8bd9316b0691696641ad25ccd72802f772
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2022-09-11 22:49:13 +0530

    Time: 48 ms (21.85%), Space: 56.3 MB (21.56%) - LeetHub

---
+++ 6-zigzag-conversion/6-zigzag-conversion.java
@@ -0,0 +1,31 @@
+class Solution {
+     public String convert(String s, int numRows) {
+        int l = s.length();
+        String c = "";
+        if (numRows == 1) {
+            return s;
+        }
+	
+        for (int i = 0; i < numRows; i++) {
+            if (i == 0 || i == numRows - 1) {
+                for (int j = i; j < l; j += 2 * (numRows - 1)) {
+                    c = c + s.charAt(j);
+                }
+            } else {
+		
+                int j = i;
+                while (j < l) {
+                    c = c + s.charAt(j);
+                    j += 2 * (numRows - i - 1);
+                    if (j >= l) {
+                        break;
+                    }
+                    c = c + s.charAt(j);
+                    j += 2 * i;
+                }
+            }
+        }
+        return c;
+    }
+	
+}
