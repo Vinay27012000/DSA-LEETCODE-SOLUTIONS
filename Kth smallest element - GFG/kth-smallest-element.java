34789bb982f1de5dcf0f22e05c0fb18cb548a4d0
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2022-10-20 23:06:26 +0530

    Added solution - LeetHub

---
+++ Kth smallest element - GFG/kth-smallest-element.java
@@ -0,0 +1,93 @@
+//{ Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class GFG {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		PrintWriter out = new PrintWriter(System.out);
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+		out.flush();
+	}
+}
+
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+ 
+  public static int partition(int[] arr, int l, int r)
+    {
+        int x = arr[r], i = l;
+        for (int j = l; j <= r - 1; j++) {
+            if (arr[j] <= x) {
+ 
+                // Swapping arr[i] and arr[j]
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+ 
+                i++;
+            }
+        }
+ 
+        // Swapping arr[i] and arr[r]
+        int temp = arr[i];
+        arr[i] = arr[r];
+        arr[r] = temp;
+ 
+        return i;
+    }
+ 
+    // This function returns k'th smallest element
+    // in arr[l..r] using QuickSort based method.
+    // ASSUMPTION: ALL ELEMENTS IN ARR[] ARE DISTINCT
+      public static int kthSmallest(int[] arr, int l, int r, int K)
+    {
+        // If k is smaller than number of elements
+        // in array
+        if (K > 0 && K <= r - l + 1) {
+ 
+            // Partition the array around last
+            // element and get position of pivot
+            // element in sorted array
+            int pos = partition(arr, l, r);
+ 
+            // If position is same as k
+            if (pos - l == K - 1)
+                return arr[pos];
+ 
+            // If position is more, recur for
+            // left subarray
+            if (pos - l > K - 1)
+                return kthSmallest(arr, l, pos - 1, K);
+ 
+            // Else recur for right subarray
+            return kthSmallest(arr, pos + 1, r,
+                               K - pos + l - 1);
+        }
+ 
+        // If k is more than number of elements
+        // in array
+        return Integer.MAX_VALUE;
+    }
+}
