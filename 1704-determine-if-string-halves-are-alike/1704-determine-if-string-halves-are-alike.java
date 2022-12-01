1dad49e873841d121a0944d474755d211901fcd6
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2022-12-01 22:25:02 +0530

    Time: 2 ms (99.29%), Space: 40.5 MB (92.95%) - LeetHub

--- 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
+++ 1704-determine-if-string-halves-are-alike/1704-determine-if-string-halves-are-alike.java
@@ -1,20 +1,27 @@
 class Solution {
-    public boolean halvesAreAlike(String s) {
-        s=s.toLowerCase();
-        String a=s.substring(0,s.length()/2);
-        String b=s.substring(s.length()/2,s.length());
-        int counter=0;
-        for(int i=0;i<a.length();i++){
-            if(a.charAt(i)=='a'||a.charAt(i)=='u'||a.charAt(i)=='i'||a.charAt(i)=='e'||a.charAt(i)=='o'){
-                counter++;
-            }
-            if(b.charAt(i)=='a'|| b.charAt(i)=='u'|| b.charAt(i)=='i'|| b.charAt(i)=='e'|| b.charAt(i)=='o'){
-                counter--;
+    public boolean halvesAreAlike(String str) {
+        int n=str.length();
+        int count=0;
+        for(int i=0;i<n/2;i++){
+            if(isVowel(str.charAt(i))){
+                count++;
             }
         }
-        if(counter==0)
-        return true;
-    return false;
+        for(int i=n/2;i<n;i++){
+            if(isVowel(str.charAt(i))){
+                count--;
+            }
+        }
+        if(count==0){
+            return true;
+        }
+        return false;
     }
-    
+    public boolean isVowel(char ch){
+        if(ch=='a' ||ch=='e' ||ch=='i' ||ch=='o' ||ch=='u'
+            ||ch=='A'||ch=='E' || ch=='I' || ch=='O' || ch=='U'){
+                return true;
+            }
+            return false;
+    }
 }
