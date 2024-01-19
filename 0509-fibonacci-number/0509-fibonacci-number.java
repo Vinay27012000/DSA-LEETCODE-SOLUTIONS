6e981003ed1c7a28e2b9e8ddd3e3cf781f0999bb
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2024-01-19 14:25:45 +0530

    Time: 0 ms (100.00%), Space: 40.6 MB (13.99%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,29 +1,41 @@
 class Solution {
-    static int fibo(int n,int []dp)
-    {
-        if(n==0)
-            return 0;
-        if(n==1)
-            return 1;
-        if(dp[n]!=-1)
-            return dp[n];
-        return fibo(n-1,dp)+fibo(n-1,dp);
-    }
+    // static int fibo(int n,int []dp)
+    // {
+    //     if(n==0)
+    //         return 0;
+    //     if(n==1)
+    //         return 1;
+    //     if(dp[n]!=-1)
+    //         return dp[n];
+    //     return fibo(n-1,dp)+fibo(n-1,dp);
+    // }
     public int fib(int n) {
-        int dp[]=new int[n+1];
+//         int dp[]=new int[n+1];
        
-        Arrays.fill(dp,-1);
+//         Arrays.fill(dp,-1);
          
          if(n== 0)
              return 0;
         if(n==1)
             return 1;
         
-         for(int i=2;i<=n;i++){
-             dp[i]=fibo(i-1,dp)+fibo(i-2,dp);
-         }
+//          for(int i=2;i<=n;i++){
+//              dp[i]=fibo(i-1,dp)+fibo(i-2,dp);
+//          }
         
-        return dp[n];
+//         return dp[n];
         
+        
+//         memory efficient code
+        
+        int prev1=0;
+        int prev2=1;
+        int curr;
+        for(int i=2;i<=n;i++){
+            curr=prev1+prev2;
+            prev1=prev2;
+            prev2=curr;
+        }
+        return prev2;
     }
 }
