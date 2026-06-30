3c290ae0971f421d73793ad9e9142b8b2ec2aaab
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2026-06-30 18:19:43 +0530

    Time: 2 ms (84.24%), Space: 47.7 MB (14.1%) - LeetHub - Time: O(n), Space: O(1)

--- 0941-valid-mountain-array/0941-valid-mountain-array.java
+++ 0941-valid-mountain-array/0941-valid-mountain-array.java
@@ -1,19 +1,25 @@
 class Solution {
     public boolean validMountainArray(int[] arr) {
         if(arr.length<3) return false;
-        boolean mid = false;
-        if((arr[0] >= arr[1])) return false;
+        // boolean mid = false;
+        // if((arr[0] >= arr[1])) return false;
 
-        for (int i=1;i<arr.length-1;i++){
-            if (arr[i] == arr[i+1]) return false;
-            if ((arr[i]>arr[i+1]) && (mid == false)){
-                mid = true;
-            }
-            if (mid) {
-                if(arr[i]<arr[i+1]) return false;
-            }
-        }
-        return mid;
+        // for (int i=1;i<arr.length-1;i++){
+        //     if (arr[i] == arr[i+1]) return false;
+        //     if ((arr[i]>arr[i+1]) && (mid == false)){
+        //         mid = true;
+        //     }
+        //     if (mid) {
+        //         if(arr[i]<arr[i+1]) return false;
+        //     }
+        // }
+        // return mid;
+    
+        int l=0,r=arr.length-1;
+        while(l+1 < arr.length-1 && arr[l]<arr[l+1])l++;
+        while(r-1 > 0 && arr[r]<arr[r-1]) r--;
+            
+    return l==r;
     }
 }
 
