df8055cb7f8e9b67a32e5bd27d2ee14e13d8b03e
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2026-07-03 13:11:05 +0530

    Time: 10 ms (17.1%), Space: 45.2 MB (96.3%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.java
@@ -1,23 +1,20 @@
 class Solution {
     public int[] smallerNumbersThanCurrent(int[] nums) {
-        int[] count = new int[101];
-        int[] res = new int[nums.length];
-        
-        for (int i =0; i < nums.length; i++) {
-            count[nums[i]]++;
+        int []result = new int[nums.length];
+        for(int i=0;i<nums.length;i++){
+            int num = nums[i];
+            for(int j=0;j<nums.length;j++){
+                if (i != j) {
+                    if (nums[j]<num){
+                        result[i]++;
+                 }
+                }
+            }
         }
-        
-        for (int i = 1 ; i <= 100; i++) {
-            count[i] += count[i-1];    
-        }
-        
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] == 0)
-                res[i] = 0;
-            else 
-                res[i] = count[nums[i] - 1];
-        }
-        
-        return res;        
+        return result;
     }
 }
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
