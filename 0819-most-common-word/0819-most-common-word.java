9223d3ebef97ae1a8d5e4498b3e464baa9968ab6
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2026-07-08 18:00:14 +0530

    Time: 13 ms (78.95%), Space: 44.1 MB (84.48%) - LeetHub - Time: O(?), Space: O(?)

--- 0819-most-common-word/0819-most-common-word.java
+++ 0819-most-common-word/0819-most-common-word.java
@@ -2,28 +2,37 @@ class Solution {
     public String mostCommonWord(String paragraph, String[] banned) {
 
         Map<String, Integer> wordMap = new HashMap<>();
-
+        Set<String> setA = new HashSet<>();
+        for(String str : banned) {
+            setA.add(str);
+        }
+         String result = null;
+        int max = -1;
       for (String word : paragraph.toLowerCase().split("[^a-z]+")) {
-            if (!word.isEmpty()) {
+            if (!word.isEmpty() && !setA.contains(word)) {
                 wordMap.put(word, wordMap.getOrDefault(word, 0) + 1);
+            
+            if (max < wordMap.get(word)) {
+                max = wordMap.get(word);
+                result = word;
+            }
             }
         }
 
-        for (int i = 0; i < banned.length; i++) {
-            if (wordMap.containsKey(banned[i])) {
-                wordMap.remove(banned[i]);
-            }
-        }
+        // for (int i = 0; i < banned.length; i++) {
+        //     if (wordMap.containsKey(banned[i])) {
+        //         wordMap.remove(banned[i]);
+        //     }
+        // }
 
-        String result = null;
-        int max = Integer.MIN_VALUE;
+       
 
-        for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
-            if (entry.getValue() > max) {
-                max = entry.getValue();
-                result = entry.getKey();
-            }
-        }
+        // for (Map.Entry<String, Integer> entry : wordMap.entrySet()) {
+        //     if (entry.getValue() > max) {
+        //         max = entry.getValue();
+        //         result = entry.getKey();
+        //     }
+        // }
 
         return result;
     }
