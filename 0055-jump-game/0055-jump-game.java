3d191a4114a57ccae543b22691d28010cdb30259
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2026-07-20 18:05:01 +0530

    Time: 2 ms (89.01%), Space: 48 MB (23.92%) - LeetHub - Time: O(n), Space: O(1)

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+       int reachable = 0;
+       for(int i = 0; i < nums.length; i ++) {
+           if(i > reachable) return false;
+           reachable = Math.max(reachable, i + nums[i]);
+       } 
+       return true;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
