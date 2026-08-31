92a0790941f739e4fae683cda7f9e4be2623710d
VINAY GUPTA <95950864+Vinay27012000@users.noreply.github.com>
2026-08-31 13:09:43 +0530

    Time: 5 ms (99.93%), Space: 78.4 MB (81.48%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.java
@@ -1,50 +1,76 @@
-// class Solution {
-//     public int longestConsecutive(int[] nums) {
-//         int count = 1;
-//         int ans=0;
-//         Set<Integer> set = new HashSet<>();
-
-//         for(int n : nums){
-//             set.add(n);
-//         }
-//         for(int n : set){
-//             if(!set.contains(n-1)) {
-//                 int curr = n;
-           
-//                 count = 1;
-//                 while(set.contains(curr+1)){
-//                 count++;
-//                 curr++;
-//                 }
-//             }
-            
-//         ans = Math.max(ans, count);
-//         }
-//         return ans;
-//     }
-// }
 class Solution {
     public int longestConsecutive(int[] nums) {
-        if(nums.length==0){
+         if (nums.length == 0) {
             return 0;
         }
-        Arrays.sort(nums);
-        int max=1;
-        int count=1;
-        for(int i=1;i<nums.length;i++){
-            if(nums[i]==nums[i-1]){
-                continue;
-            }
-            if(nums[i]==nums[i-1]+1){
-                count++;
-            }else{
-                max = Math.max(max,count);
-                count=1;
-            }
+        int min = nums[0];
+        int max = nums[0];
+
+        for (int num : nums){
+            max = Math.max(num, max);
+            min = Math.min(num, min);
         }
-        return Math.max(max,count);
+
+        if (max < 100000) {
+            boolean [] apariciones = new boolean [max - min + 1];
+            for (int num : nums) {
+                apariciones[num - min] = true;
+            }
+
+            int maxTotal = 0;
+            int maxLocal = 0;
+            for (boolean a : apariciones) {
+                if (a) {
+                    maxLocal++;
+                } else {
+                    maxTotal = Math.max(maxTotal, maxLocal);
+                    maxLocal = 0;
+                }
+            }
+            maxTotal = Math.max(maxTotal, maxLocal);
+            return maxTotal;
+        } else {
+            Set<Integer> uniNums = new HashSet();
+            for( int num : nums ){
+                uniNums.add(num);
+            }
+
+            int maxB = 0;
+            for ( int num : uniNums ){
+                if (!uniNums.contains(num - 1)){
+                    int endSeq = num;
+                    while (uniNums.contains(endSeq+1)){
+                        endSeq++;
+                    }
+                    maxB = Math.max(maxB, endSeq - num + 1);
+                }
+            }
+            return maxB;
+        }
     }
 }
+// class Solution {
+//     public int longestConsecutive(int[] nums) {
+//         if(nums.length==0){
+//             return 0;
+//         }
+//         Arrays.sort(nums);
+//         int max=1;
+//         int count=1;
+//         for(int i=1;i<nums.length;i++){
+//             if(nums[i]==nums[i-1]){
+//                 continue;
+//             }
+//             if(nums[i]==nums[i-1]+1){
+//                 count++;
+//             }else{
+//                 max = Math.max(max,count);
+//                 count=1;
+//             }
+//         }
+//         return Math.max(max,count);
+//     }
+// }
 
 // Synced seamlessly with LeetHub Pro
 // Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
